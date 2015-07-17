015489f68d388f75c5297e593807e9ea1cfe8b31
mewlist <mewlist@mewlist.com>
2015-07-17 19:12:25 +0900

    Auto generate test databases.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,23 @@ require 'active_record'
 require 'active_record/base'
 require File.expand_path('../../lib/sengiri', __FILE__)
 require File.expand_path('../../lib/sengiri/model/base', __FILE__)
+
+require "sqlite3"
+
+def setup_test_database(path)
+  # Open a database
+  db = SQLite3::Database.new path
+
+  # Create a database
+  rows = db.execute <<-SQL
+    drop table if exists sengiri_models;
+  SQL
+  rows = db.execute <<-SQL
+    create table sengiri_models ( id int PRIMARY KEY, name varchar(30));
+  SQL
+end
+
+setup_test_database "spec/db/sengiri_shard_1.sqlite3"
+setup_test_database "spec/db/sengiri_shard_2.sqlite3"
+setup_test_database "spec/db/sengiri_shard_secondary_1.sqlite3"
+setup_test_database "spec/db/sengiri_shard_secondary_2.sqlite3"
